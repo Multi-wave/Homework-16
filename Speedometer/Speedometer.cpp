3ba544aaa088738a1d43e5b2636cc4c4c71e4c63
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-12-01 20:08:01 +0400

    Create Speedometer.cpp

---
+++ Speedometer/Speedometer.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    float speed = 0;
+    float delta;
+    do {
+        std::cout << "Your speed = " << speed << "km/h. Enter a delta of speed: ";
+        std::cin >> delta;
+        speed += delta;
+        if (speed > 150) {
+            std::cout << "Max speed can't be higher 150 km/h!" << std::endl;
+            speed = 150;
+        }
+    } while (speed - 0.1 > 0);
+    std::cout << "You stopped.";
+}
