fe8b211af6e56fb475889222e31f0955baf893db
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-12-01 21:01:32 +0400

    Create Stapler.cpp

---
+++ Stapler/Stapler.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+int main()
+{
+    double whole;
+    std::string str;
+    std::cout << "Enter a whole part of number: ";
+    std::cin >> whole;
+    std::cout << "Enter a fractional part of number: ";
+    std::cin >> str;
+    int temp = stoi(str);
+    whole += (double)temp/pow(10,str.size());
+    std::cout << std::setprecision(13) <<  "Number = " << whole;
+}
