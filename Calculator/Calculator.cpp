21a4589422b3d57b2bb6ef7d9c2590b3869bb510
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-12-04 22:00:50 +0400

    Create Calculator.cpp

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+double parse(string str, int i, int part) {
+    string number;
+    if (part == 1) number = str.substr(0, i);
+    else number = str.substr(i + 1, str.length() - 1);
+    return stod(number);
+}
+
+int main()
+{
+    string example;
+    double number1;
+    double number2;
+    double answer;
+    cout << "Enter en example to calculate:" << endl;
+    cin >> example;
+    int i = 0;
+    for (i = 0; i < example.length(); ++i) {
+        if (example[i] < '0' && example[i] != '.') break;
+    }
+    number1 = parse(example, i, 1);
+    number2 = parse(example, i, 2);
+    if (example[i] == '+') answer = number1 + number2;
+    else if (example[i] == '-') answer = number1 - number2;
+    else if (example[i] == '*') answer = number1 * number2;
+    else answer = number1 / number2;
+    cout << "Answer is: " << answer;
+}
